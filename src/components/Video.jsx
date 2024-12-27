197a5574dadcf7097503037e17e437fa4d563e23
sandhyasawalkar1999 <sandhyasawalkar1999@gmail.com>
2024-12-27 13:02:36 +0530

    fix

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Time from "../loader/Time";
+// import Time from "../loader/Time";
+import Time from "./Loader/Time";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 import { abbreviateNumber } from "js-abbreviation-number";
 
