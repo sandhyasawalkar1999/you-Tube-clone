63775b10701be7b1c78693661fb7e18b1b42e58a
sandhyasawalkar1999 <sandhyasawalkar1999@gmail.com>
2024-12-27 13:06:25 +0530

    fix

--- src/components/SearchCard.jsx
+++ src/components/SearchCard.jsx
@@ -1,7 +1,7 @@
 import { abbreviateNumber } from "js-abbreviation-number";
 import React from "react";
 import { BsFillCheckCircleFill } from "react-icons/bs";
-import Time from "../loader/Time";
+import Time from "./Loader/Time";
 import { Link } from "react-router-dom";
 
 function SearchCard({ video }) {
